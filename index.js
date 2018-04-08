cbd3faf5e322fa9c51d4e3c7dd13e960fbb0da18
Patrick Gray <graypj826@gmail.com>
2018-04-08 20:37:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday,name) {
   return "Happy ${holiday}, ${name}!"
   
+<<<<<<< HEAD
 }
 
 function holidayCountdown (days, holiday){
   return "It's ${days} days until ${holiday}!"
+}
+
+
 }
 
